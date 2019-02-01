95416f9f854b8d08ab810702124b6da8cbaa8e6f
ThinhNgVHust <thinhbka.nguyenvan@gmail.com>
2019-02-01 18:39:53 +0700

    Thinh

--- Arr2DDimentions/src/Arr2DDimentions.java
+++ Arr2DDimentions/src/Arr2DDimentions.java
@@ -3,7 +3,11 @@ public class Arr2DDimentions {
 	private int arr2D[][];
 
 	public Arr2DDimentions() {
-		this.arr2D = new int[][] { { 1, 2, 3, 4, 2 }, { 1, 4, 3, 2, 5 }, { 1, 2, 5, 4, 3 }, { 2, 4, 1, 3, 5 } };
+		this.arr2D = new int[][] { 
+			{ 1, 2, 3, 4, 2 }, 
+			{ 1, 4, 3, 2, 5 }, 
+			{ 1, 2, 5, 4, 3 }, 
+			{ 2, 4, 1, 3, 5 } };
 	}
 
 	public void duyet() {
